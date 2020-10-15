eaef409c92f35f437b6f7b7d87a40b3023d9cc43
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-10-15 22:42:51 +0900

    Add coordinate system

--- sandbox2d.py
+++ sandbox2d.py
@@ -2,6 +2,11 @@ import pygame as pg
 import os
 
 
+def culculate_coordinates(camera_position, block_position):
+    return ((block_position[0] - camera_position[0]) * 16,
+            (block_position[1] - camera_position[1]) * 16)
+
+
 # Life entities
 class Player():
     pass
@@ -29,8 +34,10 @@ class Zombie(Enemy):
 
 # Block entities.
 class Block():
-    def __init__(self, screen, image_path, position, is_collision):
+    def __init__(self, screen, camera_position,
+                 image_path, position, is_collision):
         self.screen = screen
+        self.camera_position = camera_position
         self.image_path = image_path
         self.position = position
         self. is_collision = is_collision
@@ -41,12 +48,14 @@ class Block():
         return pg.image.load(self.image_path).convert()
 
     def set_block(self):
-        self.screen.blit(self.surface, [n*16 for n in self.position])
+        self.screen.blit(self.surface, culculate_coordinates(
+            self.camera_position, self.position))
 
 
 class DevBlock(Block):
-    def __init__(self, screen, position):
-        super().__init__(screen, os.path.join("assets", "dev_block.png"),
+    def __init__(self, screen, camera_position, position):
+        super().__init__(screen, camera_position,
+                         os.path.join("assets", "dev_block.png"),
                          position, True)
 
 
@@ -59,13 +68,28 @@ def main():
     pg.init()
     screen = pg.display.set_mode((256, 256))
     blocks = []
-    for i in range(16):
-        for j in range(16):
-            blocks.append(DevBlock(screen, (i, j)))
+    camera_position = (-5, 0)
     while True:
+        screen.fill((0, 0, 0))
+        for i in range(16):
+            for j in range(16):
+                blocks.append(DevBlock(screen, camera_position, (i, j)))
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_w:
+                    camera_position = (
+                        camera_position[0], camera_position[1] - 1)
+                if event.key == pg.K_a:
+                    camera_position = (
+                        camera_position[0] - 1, camera_position[1])
+                if event.key == pg.K_s:
+                    camera_position = (
+                        camera_position[0], camera_position[1] + 1)
+                if event.key == pg.K_d:
+                    camera_position = (
+                        camera_position[0] + 1, camera_position[1])
 
         pg.display.update()
 
