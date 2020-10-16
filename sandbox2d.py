f85714f80c4909f9292283c4e9333cef530761a2
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-10-17 08:19:53 +0900

    Refactoring draw block system in sandbox2d.py

--- sandbox2d.py
+++ sandbox2d.py
@@ -35,27 +35,28 @@ def culculate_coordinates(camera_position, block_position):
             (-(block_position[1] - camera_position[1] - 15) * 16))
 
 
-def by_id_make_block(id, screen, camera_position, block_position):
+def by_id_make_block(id, block_position):
     if id == DEV_BLOCK_BLUE_ID:
-        return DevBlockBlue(screen, camera_position, block_position)
+        return DevBlockBlue(block_position)
     elif id == DEV_BLOCK_AQUA_ID:
-        return DevBlockAqua(screen, camera_position, block_position)
+        return DevBlockAqua(block_position)
 
 
 class Chunk():
     def __init__(self, chunk_data, chunk_position):
         self.chunk_data = chunk_data
         self.chunk_position = chunk_position
-
-    def draw_chunk(self, screen, camera_position):
         self.blocks = []
         for i in range(CHUNK_HEIGHT):
             for j in range(CHUNK_WIDTH):
                 self.blocks.append(
                     by_id_make_block(self.chunk_data[i][j],
-                                     screen, camera_position,
                                      (j + self.chunk_position * CHUNK_WIDTH,
                                       (CHUNK_HEIGHT / 2 - 1) - i)))
+
+    def draw_chunk(self, screen, camera_position):
+        for i in self.blocks:
+            i.draw_block(screen, camera_position)
 
 
 # Life entities
@@ -85,35 +86,29 @@ class Zombie(Enemy):
 
 # Block entities.
 class Block():
-    def __init__(self, screen, camera_position,
-                 image_path, position, is_collision):
-        self.screen = screen
-        self.camera_position = camera_position
+    def __init__(self, image_path, position, is_collision):
         self.image_path = image_path
         self.position = position
         self. is_collision = is_collision
         self.surface = self.load_image()
-        self.set_block()
 
     def load_image(self):
         return pg.image.load(self.image_path).convert()
 
-    def set_block(self):
-        self.screen.blit(self.surface, culculate_coordinates(
-            self.camera_position, self.position))
+    def draw_block(self, screen, camera_position):
+        screen.blit(self.surface, culculate_coordinates(
+            camera_position, self.position))
 
 
 class DevBlockBlue(Block):
-    def __init__(self, screen, camera_position, position):
-        super().__init__(screen, camera_position,
-                         os.path.join("assets", "dev_block_blue.png"),
+    def __init__(self, position):
+        super().__init__(os.path.join("assets", "dev_block_blue.png"),
                          position, True)
 
 
 class DevBlockAqua(Block):
-    def __init__(self, screen, camera_position, position):
-        super().__init__(screen, camera_position,
-                         os.path.join("assets", "dev_block_aqua.png"),
+    def __init__(self, position):
+        super().__init__(os.path.join("assets", "dev_block_aqua.png"),
                          position, True)
 
 
@@ -126,7 +121,7 @@ def main():
     pg.init()
     screen = pg.display.set_mode((256, 256))
     camera_position = (0, 0)
-    move_speed = 0.5
+    move_speed = 0.01
     chunk = Chunk(chank_data_sample, 0)
     chunk_2 = Chunk(chank_data_sample_2, 1)
     chunk_3 = Chunk(chank_data_sample, 2)
